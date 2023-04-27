fc2fd800852d51076f8a2a93991022099486e210
kevin <kevin9000MG@gmail.com>
2023-04-27 21:56:34 +0100

    adds method to add users and list tables

--- src/main/kotlin/Controllers/TableAPI.kt
+++ src/main/kotlin/Controllers/TableAPI.kt
@@ -3,5 +3,12 @@ import Models.Table
 
 class TableAPI{
     private var users = ArrayList<Table>()
-
+    fun add(Table: Table): Boolean {
+        return users.add(Table)
+    }
+    fun listAllTable(): String =
+        if (users.isEmpty()) "No users found"
+        else users.joinToString(separator = "\n") { note ->
+            users.indexOf(note).toString() + ": " + note.toString()
+        }
 }
