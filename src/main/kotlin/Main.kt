15a62743cc032d3cd5a90c2468cc9d398a7cb820
kevin <kevin9000MG@gmail.com>
2023-04-27 21:18:17 +0100

    adds to option to add users

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,38 @@
+import Models.Table
 import utils.ScannerInput
 
+import utils.ScannerInput.readNextLine
 fun main() = runMenu()
 
 fun runMenu() {
     do {
         when (val option = mainMenu()) {
-
+            1 -> createUser()
             else -> println("Invalid menu choice: $option")
         }
     } while (true)
 }
+
+fun createUser() {
+    val name = readNextLine("Enter a name for your gym schedule: ")
+    val monday = readNextLine("Is Monday a workout day for you? (y/n)").equals("y")
+    val tuesday = readNextLine("Is Tuesday a workout day for you? (y/n)").equals("y")
+    val wednesday = readNextLine("Is Wednesday a workout day for you? (y/n)").equals("y")
+    val thursday = readNextLine("Is Thursday a workout day for you? (y/n)").equals("y")
+    val friday = readNextLine("Is Friday a workout day for you? (y/n)").equals("y")
+    val saturday = readNextLine("Is Saturday a workout day for you? (y/n)").equals("y")
+    val sunday = readNextLine("Is Sunday a workout day for you? (y/n)").equals("y")
+
+    val gymSchedule = Table(name, monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+    println("""
+        ${gymSchedule}
+    """.trimIndent())
+}
+
 fun mainMenu() = ScannerInput.readNextInt(
     """ 
          > -----------------------------------------------------  
-         > 1 Add a table
+         >  Add user 
          > 
          > 
          > 
--- src/main/kotlin/Models/Table.kt
+++ src/main/kotlin/Models/Table.kt
@@ -1,3 +1,13 @@
 package Models
 
-class Table
+data class Table(
+    var name: String,
+    var monday: Boolean,
+    var tuesday: Boolean,
+    var wednesday: Boolean,
+    var thursday: Boolean,
+    var friday: Boolean,
+    var saturday: Boolean,
+    var sunday: Boolean
+)
+
