4024302fe6859f0a21ec9ac6b52aea902215cf05
kevin <kevin9000MG@gmail.com>
2023-04-30 03:59:17 +0100

    adds a option to add table to user if not done

--- src/main/kotlin/Controllers/TableAPI.kt
+++ src/main/kotlin/Controllers/TableAPI.kt
@@ -48,6 +48,15 @@ class TableAPI(serializerType: Serializer) {
         return users.size
     }
 
+    // not tested
+    fun noTables(): String =
+        if (users.isEmpty()) "No users found"
+        else users.filter { it.timetable == null }.toString()
+
+
+
+
+
     // Search by Users name // tested
     fun searchByName(searchString: String) =
         users.filter { user -> user.name.contains(searchString, ignoreCase = true) }
@@ -130,12 +139,20 @@ fun findUser(index: Int): User? {
             .count()
             .toInt()
     }
-
+// tested
     fun findNote(index: Int): Table? {
         return if (isValidTableIndex(index, tables)) {
             tables[index]
         } else null
     }
+
+    fun addTabletoUser(index: Int, table: Table): Boolean {
+        return if (isValidUserIndex(index, users)) {
+            users[index].timetable = table
+            true
+        } else false
+    }
+
 }
 
 
