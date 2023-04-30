a271825a569d3d0f19d02d2b1595ea67600bcd09
kevin <kevin9000MG@gmail.com>
2023-04-30 07:35:25 +0100

    changes doc

--- src/main/kotlin/Controllers/TableAPI.kt
+++ src/main/kotlin/Controllers/TableAPI.kt
@@ -4,16 +4,27 @@ import Models.User
 import persistence.Serializer
 
 import kotlin.collections.ArrayList
-class TableAPI(serializerType: Serializer) { /**
-     The serializer used for storing and loading data. The serializer type is determined by the serializerType parameter
-     passed to the SwimmerAPI constructor.
-     */
+
+/**
+
+This class provides an API for managing tables and users using a serializer.
+
+@property serializer An instance of the Serializer class to read and write data to disk.
+
+@property tables A list of all tables in the system.
+
+@property users A list of all users in the system.
+ */
+class TableAPI(serializerType: Serializer) {
+
     private var serializer: Serializer = serializerType
+    private var tables = ArrayList<Table>()
+    private var users = ArrayList<User>()
+
     /**
-     *
-     Loads the swimmer data from the serialized data using the currently assigned serializer. The loaded data is
-     then used to populate the tables and users lists.
-     @throws Exception If there's an error reading or deserializing the data.
+
+    Load tables and users from disk.
+    @throws Exception if an error occurs while reading data from disk.
      */
     @Throws(Exception::class)
     fun load() {
@@ -21,9 +32,9 @@ class TableAPI(serializerType: Serializer) { /**
         users = serializer.read() as ArrayList<User>
     }
     /**
-     Writes the swimmer data to a serialized format using the currently assigned serializer. The data to be written is
-     obtained from the tables and users lists.
-     @throws Exception If there's an error writing or serializing the data.
+
+    Save tables and users to disk.
+    @throws Exception if an error occurs while writing data to disk.
      */
     @Throws(Exception::class)
     fun store() {
@@ -31,37 +42,27 @@ class TableAPI(serializerType: Serializer) { /**
         serializer.write(users)
     }
     /**
-     ** A list of Table objects representing the swimmer data. The list is initially empty.
-     */
-    private var tables = ArrayList<Table>()
-    /**
-     A list of User objects representing the user data. The list is initially empty.
-     */
-    private var users = ArrayList<User>()
-    /**
-     Adds a new User object to the users list.
-     @param user The User object to be added.
-     @return true if the user was successfully added, false otherwise.
+
+    Add a user to the system.
+    @param user The user to add.
+    @return true if the user was added successfully, false otherwise.
      */
     fun addUser(user: User): Boolean {
         return users.add(user)
     }
     /**
 
-     Adds a new Table object to the tables list.
-     @param table The Table object to be added.
-     @return true if the table was successfully added, false otherwise.
+    Add a table to the system.
+    @param index The table to add.
+    @return true if the table was added successfully, false otherwise.
      */
     fun addTable(index: Table): Boolean {
         return tables.add(index)
     }
     /**
 
-     Returns a string representation of all User objects in the users list.
-     If the users list is empty, the string "No users found" is returned.
-     Otherwise, the string contains one line for each User object in the users list,
-     in the format "index: User.toString()".
-     @return A string representation of all User objects in the users list.
+    Get a string representation of all users in the system.
+    @return A string representation of all users in the system, or "No users found" if there are no users.
      */
     fun listAllUsers(): String =
         if (users.isEmpty()) "No users found"
@@ -69,23 +70,17 @@ class TableAPI(serializerType: Serializer) { /**
             users.indexOf(User).toString() + ": " + User.toString()
         }
     /**
-     Returns the total number of User objects in the users list.
-     @return The total number of User objects in the users list.
-     */
 
+    Get the total number of users in the system.
+    @return The total number of users in the system.
+     */
     fun getTotalUsers(): Int {
         return users.size
     }
-
     /**
 
-     Returns a string representation of all User objects in the users list that do not have
-     a Timetable object assigned to them.
-     If there are no such User objects in the users list, the string "No users found" is returned.
-     Otherwise, the string contains one line for each User object in the users list that
-     does not have a Timetable object assigned to it, in the format "index: User.toString()".
-     @return A string representation of all User objects in the users list that do not have
-
+    Get a string representation of all users in the system who do not have a timetable assigned to them.
+    @return A string representation of all users who do not have a timetable assigned to them, or "No users found" if there are none.
      */
     fun noTables(): String =
         if (users.isEmpty()) "No users found"
@@ -93,99 +88,75 @@ class TableAPI(serializerType: Serializer) { /**
             .joinToString(separator = "\n") { user ->
                 users.indexOf(user).toString() + ": " + user.toString()
             }
-
     /**
-     Returns a string representation of all User objects in the users list whose name
-     field contains the specified search string, ignoring case.
-     The search is case-insensitive and matches any User object whose name field contains
-     the specified search string as a substring.
-     The returned string contains one line for each User object in the users list that
-     matches the search criteria, in the format "index: User.toString()".
-     @param searchString The string to search for in the name field of User objects.
-     @return A string representation of all User objects in the users list whose name
+
+    Search for a user by name.
+    @param searchString The name to search for.
+    @return A string representation of all users whose name contains the search string, or an empty string if there are none.
      */
     fun searchByName(searchString: String) =
         users.filter { user -> user.name.contains(searchString, ignoreCase = true) }
             .joinToString(separator = "\n") { user -> users.indexOf(user).toString() + ": " + user.toString() }
     /**
 
-     Returns a string representation of all User objects in the users list whose email
-     field contains the specified search string, ignoring case.
-     The search is case-insensitive and matches any User object whose email field contains
-     the specified search string as a substring.
-     The returned string contains one line for each User object in the users list that
-     matches the search criteria, in the format "index: User.toString()".
-     @param searchString The string to search for in the email field of User objects.
-     @return A string representation of all User objects in the users list whose email
-
+    Search for a user by email.
+    @param searchString The email to search for.
+    @return A string representation of all users whose email contains the search string, or an empty string if there are none.
      */
-    fun searchByEmail(searchString: String) =
-        users.filter { user -> user.email.contains(searchString, ignoreCase = true) }
-            .joinToString(separator = "\n") { user -> users.indexOf(user).toString() + ": " + user.toString() }
-
     /**
-
-     Returns the User object at the specified index in the users list, or null if the
-     index is out of range or the users list is empty.
-     @param index The index of the User object to retrieve.
-     @return The User object at the specified index in the users list, or null if the
+     * Returns a user with the given index, if it is a valid index.
+     * @param index The index of the user to return.
+     * @return The user at the given index, or null if the index is invalid.
      */
     fun findUser(index: Int): User? {
         return if (isValidUserIndex(index, users)) {
             users[index]
         } else null
     }
+
     /**
-     Prints a string representation of the timetable of each User object in the users list.
-     If a User object has a non-null timetable field, the function prints the string
-     representation of the timetable field using the toString() method.
-     If a User object has a null timetable field, the function does nothing for that user.
-     Note: This function does not return a string; it only prints the timetables to the console.
+     * Prints the string representation of each user's timetable in the users list.
      */
     fun listAllTables() {
         for (user in users) {
             println(user.timetable?.toString())
         }
     }
-     /**
-    Determines whether the specified index is a valid index for the users list.
-    An index is valid if it is greater than or equal to 0 and less than the size of the users
-    list.
-     @param index The index to check.
-    @param users The list to check against.
-    @return true if the index is valid for the list; false otherwise.
+
+    /**
+     * Checks if the given index is valid for the given list.
+     * @param index The index to check.
+     * @param users The list to check the index against.
+     * @return True if the index is valid, false otherwise.
      */
     fun isValidUserIndex(index: Int, users: List<Any>): Boolean {
-       return (index >= 0 && index < users.size)
-
+        return (index >= 0 && index < users.size)
     }
 
     /**
-
-    Returns the number of tables currently stored in the tables list.
-    @return The number of tables in the tables list.
+     * Returns the number of tables in the tables list.
+     * @return The number of tables in the list.
      */
-        fun listnmbtables(): Int {
+    fun listnmbtables(): Int {
         return tables.size
     }
+    fun searchByEmail(searchString: String) =
+        users.filter { user -> user.email.contains(searchString, ignoreCase = true) }
+            .joinToString(separator = "\n") { user -> users.indexOf(user).toString() + ": " + user.toString() }
     /**
-
-    Determines whether the specified index is a valid index for the tables list.
-    An index is valid if it is greater than or equal to 0 and less than the size of the tables
-    list.
-    @param index The index to check.
-    @param tables The list to check against.
-    @return true if the index is valid for the list; false otherwise.
+     * Checks if the given index is valid for the given list.
+     * @param index The index to check.
+     * @param users The list to check the index against.
+     * @return True if the index is valid, false otherwise.
      */
     fun isValidTableIndex(index: Int, users: List<Any>): Boolean {
         return (index >= 0 && index < users.size)
     }
 
     /**
-
-    Returns the number of users who have the specified day marked as active in their timetable.
-    @param day The day to search for (e.g. "monday", "tuesday", etc.).
-    @return The number of users with the specified day marked as active in their timetable.
+     * Returns the number of users who have the given day as an active day in their timetable.
+     * @param day The day to check for.
+     * @return The number of users with the given day as an active day.
      */
     fun activedaymembers(day: String): Int {
         return users.stream()
@@ -205,13 +176,14 @@ class TableAPI(serializerType: Serializer) { /**
             .toInt()
     }
 
-
-     /**
-     * Returns the number of inactive members on the specified day.
-     *
-     * @param day the day of the week (e.g. "monday", "tuesday", etc.)
-     * @return the number of inactive members on the specified day
+    /**
+     * Returns the number of users who have the given day as an inactive day in their timetable.
+     * @param day The day to check for.
+     * @return The number of users with the given day as an inactive day.
      */
+
+
+
     fun inactivedaymemberss(day: String): Int {
         return users.stream()
             .filter { user: User ->
@@ -230,30 +202,25 @@ class TableAPI(serializerType: Serializer) { /**
             .toInt()
     }
 
-    fun getTable(tableName: String?): Table? {
-        return tables.find { it.name == tableName }
-    }
-    fun getUser(user: User): User? {
-        return users.find { it.name == user.name }
-    }
     /**
-     Returns the User object at the specified index in the list of users.
-     Returns null if the index is invalid.
-     @param index the index of the desired user
-     @return the User object at the specified index, or null if the index is invalid
+     * Updates the timetable of the user with the specified ID.
+     * @param userId The ID of the user to update.
+     * @param updateTable The new timetable to assign to the user.
      */
-    fun updateTable(userId: Int, updateTable: Table) {
-        // Find the user with the matching user ID
-        val user = findUser(userId)
 
-        // Update the table if the user exists
+    fun updateTable(userId: Int, updateTable: Table) {
+        val user = findUser(userId)
         user?.let { it.timetable = updateTable }
     }
-
+    /**
+     * Finds a table by index.
+     * @param index The index of the table to find.
+     * @return The table at the specified index, or null if it doesn't exist.
+     */
     fun findNote(index: Int): Table? {
         return if (isValidTableIndex(index, tables)) {
             tables[index]
         } else null
     }
 
-    }
+}
